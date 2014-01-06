bfb8dbb92034af144549e33f6b49161a7e565786
Nick Holub <nikholub@gmail.com>
2014-01-06 15:36:58 -0800

    Yeoman generated app

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('angularDashboardApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
