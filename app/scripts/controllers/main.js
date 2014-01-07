dc0fcae91ca714b6c8fd92d9d14e42c5971f58b5
Nick Holub <nikholub@gmail.com>
2014-01-07 15:01:34 -0800

    encapsulate addWidget/removeWidget into dashboard directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('app').controller('MainCtrl', function ($scope, $interval) {
+  $scope.dashboardOptions = {};
+
   $interval(function () {
     $scope.randomValue = Math.random();
   }, 500);
+
 
   var dashboardWidgets = [];
 
@@ -32,26 +35,14 @@ angular.module('app').controller('MainCtrl', function ($scope, $interval) {
 
   $scope.dashboardWidgets = dashboardWidgets;
 
-  var count = $scope.dashboardWidgets.length + 1;
-
   $scope.addWidget = function (id) {
-    $scope.dashboardWidgets.push({
-      title: 'Widget ' + count++,
-      directive: id
-    });
+    $scope.dashboardOptions.addWidget(id);
   };
 
   $scope.addWidgetScopeWatch = function () {
-    $scope.dashboardWidgets.push({
-      title: 'Widget ' + count++,
-      directive: 'scope-watch',
-      options: {
-        varName: 'randomValue'
-      }
-    });
-  };
-
-  $scope.removeWidget = function (widget) {
-    $scope.dashboardWidgets.splice(_.indexOf($scope.dashboardWidgets, widget), 1);
+    $scope.dashboardOptions.addWidget('scope-watch',
+      {
+        propertyName: 'randomValue'
+      });
   };
 });
--- app/scripts/directives/dashboard.js
+++ app/scripts/directives/dashboard.js
@@ -7,12 +7,30 @@ angular.module('app').controller('DashboardController', function ($scope) {
 
 angular.module('app').directive('dashboard', function () {
   return {
-    restrict: 'EA',
+    restrict: 'A',
     templateUrl: 'scripts/widgets/dashboard.html',
     scope: true,
     controller: 'DashboardController',
-    link: function (scope, element, attrs) {
-      scope.widgets = scope[attrs.widgets];
+    link: function (scope, element, attrs, DashboardController) {
+      scope.widgets = scope.$eval(attrs.widgets);
+      scope.options = scope.$eval(attrs.dashboard);
+
+      var count = scope.widgets.length + 1;
+
+      scope.addWidget = function (directive, options) {
+        scope.widgets.push({
+          title: 'Widget ' + count++,
+          directive: directive,
+          options: options
+        });
+      };
+
+      scope.removeWidget = function (widget) {
+        scope.widgets.splice(_.indexOf(scope.widgets, widget), 1);
+      };
+
+      // allow adding widgets externally
+      scope.options.addWidget = scope.addWidget;
     }
   };
 });
