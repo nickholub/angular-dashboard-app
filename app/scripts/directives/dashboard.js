9a15f1617701aa7b7dd92bcab3758af9c004053c
Nick Holub <nikholub@gmail.com>
2014-01-07 14:11:21 -0800

    create new scope for dashboard directive

--- app/scripts/directives/dashboard.js
+++ app/scripts/directives/dashboard.js
@@ -9,6 +9,7 @@ angular.module('app').directive('dashboard', function () {
   return {
     restrict: 'EA',
     templateUrl: 'scripts/widgets/dashboard.html',
+    scope: true,
     controller: 'DashboardController',
     link: function (scope, element, attrs) {
       scope.widgets = scope[attrs.widgets];
