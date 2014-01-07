b4e03cff7617c787af2a0f56d3e0dad9f1045608
Nick Holub <nikholub@gmail.com>
2014-01-07 13:49:31 -0800

    dashboard 'widgets' attribute

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,30 +1,28 @@
 'use strict';
 
 angular.module('app').controller('MainCtrl', function ($scope, $interval) {
-
-
   $interval(function () {
     $scope.randomValue = Math.random();
   }, 500);
 
-  var widgets = [];
+  var dashboardWidgets = [];
 
-  widgets.push({
+  dashboardWidgets.push({
     title: 'Widget 1',
     directive: 'two'
   });
 
-  widgets.push({
+  dashboardWidgets.push({
     title: 'Widget 2',
     directive: 'one'
   });
 
-  widgets.push({
+  dashboardWidgets.push({
     title: 'Widget 3',
     directive: 'two'
   });
 
-  widgets.push({
+  dashboardWidgets.push({
     title: 'Widget 4',
     directive: 'scope-watch',
     options: {
@@ -32,19 +30,19 @@ angular.module('app').controller('MainCtrl', function ($scope, $interval) {
     }
   });
 
-  $scope.widgets = widgets;
+  $scope.dashboardWidgets = dashboardWidgets;
 
-  var count = $scope.widgets.length + 1;
+  var count = $scope.dashboardWidgets.length + 1;
 
   $scope.addWidget = function (id) {
-    $scope.widgets.push({
+    $scope.dashboardWidgets.push({
       title: 'Widget ' + count++,
       directive: id
     });
   };
 
   $scope.addWidgetScopeWatch = function () {
-    $scope.widgets.push({
+    $scope.dashboardWidgets.push({
       title: 'Widget ' + count++,
       directive: 'scope-watch',
       options: {
@@ -54,6 +52,6 @@ angular.module('app').controller('MainCtrl', function ($scope, $interval) {
   };
 
   $scope.removeWidget = function (widget) {
-    $scope.widgets.splice(_.indexOf($scope.widgets, widget), 1);
+    $scope.dashboardWidgets.splice(_.indexOf($scope.dashboardWidgets, widget), 1);
   };
 });
