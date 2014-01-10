a56532f9f0a503d5b4637962325753852a5a3539
Nick Holub <nikholub@gmail.com>
2014-01-10 15:00:31 -0800

    lineChart widget

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -51,6 +51,16 @@ angular.module('app').controller('MainCtrl', function ($scope, $interval) {
       style: {
         width: '30%'
       }
+    },
+    {
+      directive: 'line-chart',
+      attrs: {
+        chart: 'chart',
+        style: 'height: 200px'
+      },
+      style: {
+        width: '50%'
+      }
     }
   ];
 
@@ -82,6 +92,41 @@ angular.module('app').controller('MainCtrl', function ($scope, $interval) {
   $scope.percentage = 10;
   $interval(function () {
     $scope.percentage = ($scope.percentage + 10) % 100;
+  }, 1000);
+
+  var max = 30;
+
+  // line chart
+  var data = [];
+  var chartValue = 50;
+  function nextValue() {
+    chartValue += Math.random() * 40 - 20;
+    chartValue = chartValue < 0 ? 0 : chartValue > 100 ? 100 : chartValue;
+    return chartValue;
+  }
+  var now = Date.now();
+  for (var i = max - 1; i >= 0; i--) {
+    data.push({
+      timestamp: now - i * 1000,
+      value: nextValue()
+    });
+  }
+  $scope.chart = {
+    data: data,
+    max: max
+  };
+
+  $interval(function () {
+    data.shift();
+    data.push({
+      timestamp: Date.now(),
+      value: nextValue()
+    });
+
+    $scope.chart = {
+      data: data,
+      max: max
+    };
   }, 1000);
 
   // external controls
---
+++ app/scripts/widgets/lineChart/lineChart.js
@@ -0,0 +1,61 @@
+'use strict';
+
+angular.module('ui.dashboard.widgets')
+  .directive('lineChart', function () {
+    return {
+      template: '<div></div>',
+      scope: {
+        chart: '='
+      },
+      replace: true,
+      link: function postLink(scope, element) {
+        console.log('link');
+        var lineChart = new google.visualization.LineChart(element[0]);
+
+        function draw(chart) {
+          var data = chart.data;
+
+          var table = new google.visualization.DataTable();
+          table.addColumn('datetime');
+          table.addColumn('number');
+          table.addRows(data.length);
+
+          var view = new google.visualization.DataView(table);
+
+          for (var i = 0; i < data.length; i++) {
+            var item = data[i];
+            table.setCell(i, 0, new Date(item.timestamp));
+            var value = parseFloat(item.value);
+            table.setCell(i, 1, value);
+          }
+
+          var lastTimestamp;
+
+          if (data.length) {
+            var last = data[data.length - 1];
+            lastTimestamp = last.timestamp;
+          } else {
+            lastTimestamp = Date.now();
+          }
+
+          var max = new Date(lastTimestamp);
+          var min = new Date(lastTimestamp - (chart.max - 1) * 1000);
+
+          var chartOptions = {
+            legend: 'none',
+            vAxis: { minValue: 0, maxValue: 100 },
+            hAxis: { viewWindow: { min: min, max: max }},
+            //chartArea: { top: 20, left: 30, height: 240 }
+          };
+
+          lineChart.draw(view, chartOptions);
+        }
+
+        scope.$watch('chart', function (chart) {
+          if (chart && chart.data && chart.max) {
+            draw(chart);
+          }
+        });
+      }
+    };
+  });
