a9167684520a7a6af1b7121dab2cf20fa59c927b
Nick Holub <nikholub@gmail.com>
2014-01-19 21:40:08 -0800

    application Node.js server

--- app/settings.js
+++ app/settings.js
@@ -1,5 +1,5 @@
 // client-side settings (for dev only)
 window.settings = {};
-settings.webSocketURL = 'ws://node1.morado.com:9090/pubsub';
+settings.webSocketURL = 'ws://localhost:3000/sockjs/websocket';
 settings.randomValueTopic = 'randomValue';
 settings.topNTopic = 'demos.twitter.topURLs';
