ae1b176bfb082215ef70cc7f92d9ce6a4e31d873
Nick Holub <nikholub@gmail.com>
2014-01-22 16:52:02 -0800

    Notification Service with PNotify (Bootstrap 3)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,16 +3,16 @@
 angular.module('ui.dashboard.widgets', ['ngGrid']);
 
 angular.module('app', [
-  'app.service',
-  'app.websocket',
-  'ngRoute',
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ui.dashboard',
-  'nvd3ChartDirectives',
-  'ui.dashboard.widgets'
-])
+    'app.service',
+    'app.websocket',
+    'ngRoute',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ui.dashboard',
+    'nvd3ChartDirectives',
+    'ui.dashboard.widgets'
+  ])
   .constant('settings', window.settings)
   .config(function ($routeProvider, webSocketProvider, settings) {
     if (settings) {
@@ -32,6 +32,6 @@ angular.module('app', [
         redirectTo: '/'
       });
 
-      // Use Bootstrap 3 theme in PNotify jQuery plugin
-      jQuery.pnotify.defaults.styling = 'bootstrap3';
+    // Use Bootstrap 3 theme in PNotify jQuery plugin
+    jQuery.pnotify.defaults.styling = 'bootstrap3';
   });
