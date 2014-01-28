ad69a5a4adafa28ab504e7733a39da4e3fc0d0d5
Nick Holub <nikholub@gmail.com>
2014-01-28 14:26:16 -0800

    progress WebSocket topic

--- config.js
+++ config.js
@@ -5,17 +5,18 @@ config.port = process.env.PORT || 3000;
 // client settings (passed to the browser)
 config.settings = {};
 var settings = config.settings;
-settings.webSocketURL = 'ws://localhost:3000/sockjs/websocket';
+settings.webSocketURL = 'ws://localhost:9090/pubsub';
 settings.randomValueTopic = 'randomValue';
 settings.topNTopic = 'demos.twitter.topURLs';
 
 settings.topic = {};
 settings.topic.visualdata = {};
 
-settings.topic.visualdata.piValue = 'app.visualdata.piValue_{"type":"simple"}';
-settings.topic.visualdata.percentage = 'app.visualdata.percentage_{"type":"percentage"}';
-settings.topic.visualdata.chartValue = 'app.visualdata.chartValue_{"type":"timeseries","minValue":0,"maxValue":100}';
-settings.topic.visualdata.chartValue2 = 'app.visualdata.chartValue2_{"type":"timeseries","minValue":0,"maxValue":100}';
-settings.topic.visualdata.topn = 'app.visualdata.topn_{"type":"topN","n":10}';
+settings.topic.visualdata.piValue = 'visualdata.piValue';
+settings.topic.visualdata.percentage = 'visualdata.percentage';
+settings.topic.visualdata.progress = 'visualdata.progress';
+settings.topic.visualdata.chartValue = 'visualdata.chartValue';
+settings.topic.visualdata.chartValue2 = 'visualdata.chartValue2';
+settings.topic.visualdata.topn = 'visualdata.topn';
 
 module.exports = config;
