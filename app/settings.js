bce74f7ca20b3f2963c9d6874cab69216661c654
Nick Holub <nikholub@gmail.com>
2014-01-30 12:13:00 -0800

    app config

--- app/settings.js
+++ app/settings.js
@@ -1,15 +1,17 @@
 // client-side settings (for dev only)
 window.settings = {};
-settings.webSocketURL = 'ws://localhost:3000/sockjs/websocket';
-settings.randomValueTopic = 'randomValue';
-settings.topNTopic = 'demos.twitter.topURLs';
+
+settings.gatewayHost = 'localhost:9090';
+
+settings.webSocketURL = 'ws://' + settings.gatewayHost + '/pubsub';
+settings.restBaseURL = 'http://' + settings.gatewayHost + '/ws/v1/';
 
 settings.topic = {};
 settings.topic.visualdata = {};
 
-settings.topic.visualdata.piValue = 'visualdata.piValue';
-settings.topic.visualdata.percentage = 'visualdata.percentage';
-settings.topic.visualdata.progress = 'visualdata.progress';
-settings.topic.visualdata.chartValue = 'visualdata.chartValue';
-settings.topic.visualdata.chartValue2 = 'visualdata.chartValue2';
-settings.topic.visualdata.topn = 'visualdata.topn';
+settings.topic.visualdata.piValue = 'piValue';
+settings.topic.visualdata.percentage = 'percentage';
+settings.topic.visualdata.progress = 'progress';
+settings.topic.visualdata.chartValue = 'chartValue';
+settings.topic.visualdata.chartValue2 = 'chartValue2';
+settings.topic.visualdata.topn = 'topn';
--- config.js
+++ config.js
@@ -1,24 +1,24 @@
 var config = {};
 
-config.port = process.env.PORT || 3000;
+config.port = process.env.PORT || 3005;
 
 // client settings (passed to the browser)
 config.settings = {};
 var settings = config.settings;
-settings.webSocketURL = 'ws://localhost:9090/pubsub';
-settings.restBaseURL = 'http://localhost:9090/ws/v1/';
 
-settings.randomValueTopic = 'randomValue';
-settings.topNTopic = 'demos.twitter.topURLs';
+settings.gatewayHost = process.env.GATEWAY_HOST || 'localhost:9090';
+
+settings.webSocketURL = 'ws://' + settings.gatewayHost + '/pubsub';
+settings.restBaseURL = 'http://' + settings.gatewayHost + '/ws/v1/';
 
 settings.topic = {};
 settings.topic.visualdata = {};
 
-settings.topic.visualdata.piValue = 'visualdata.piValue';
-settings.topic.visualdata.percentage = 'visualdata.percentage';
-settings.topic.visualdata.progress = 'visualdata.progress';
-settings.topic.visualdata.chartValue = 'visualdata.chartValue';
-settings.topic.visualdata.chartValue2 = 'visualdata.chartValue2';
-settings.topic.visualdata.topn = 'visualdata.topn';
+settings.topic.visualdata.piValue = 'piValue';
+settings.topic.visualdata.percentage = 'percentage';
+settings.topic.visualdata.progress = 'progress';
+settings.topic.visualdata.chartValue = 'chartValue';
+settings.topic.visualdata.chartValue2 = 'chartValue2';
+settings.topic.visualdata.topn = 'topn';
 
 module.exports = config;
