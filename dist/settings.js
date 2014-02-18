4b35b47f70b5662003aa7e725c46fafd4ea4644b
Nick Holub <nikholub@gmail.com>
2014-02-18 12:59:49 -0800

    Distribution files

---
+++ dist/settings.js
@@ -0,0 +1,21 @@
+// client-side settings (for dev only)
+window.settings = {};
+
+//settings.gatewayHost = 'localhost:9090';
+settings.gatewayHost = 'node0.morado.com:9090';
+settings.meteorHost = 'localhost:3000';
+
+settings.webSocketURL = 'ws://' + settings.gatewayHost + '/pubsub';
+settings.restBaseURL = 'http://' + settings.gatewayHost + '/ws/v1/';
+settings.meteorURL = 'ws://' + settings.meteorHost + '/websocket';
+
+settings.topic = {};
+settings.topic.visualdata = {};
+
+settings.topic.visualdata.piValue = 'piValue';
+settings.topic.visualdata.percentage = 'percentage';
+settings.topic.visualdata.progress = 'progress';
+settings.topic.visualdata.chartValue = 'chartValue';
+settings.topic.visualdata.chartValue2 = 'chartValue2';
+settings.topic.visualdata.topn = 'topn';
+settings.topic.visualdata.pieChart = 'piechart';
